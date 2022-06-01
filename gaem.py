48598c4aaba3527d4ae1b9d42631a536955684ce
Niksld <44138297+Niksld@users.noreply.github.com>
2022-06-01 10:05:44 +0200

    Updates
    no neg. values anymore
    hitbox now works
    .gitignore added

--- gaem.py
+++ gaem.py
@@ -1,10 +1,11 @@
 from tkinter import Canvas, Scale, Tk, Toplevel, Button
 from random import choice, randrange
+from time import sleep
 import Objects
 
 W_WIDTH = 1200
 W_HEIGHT = 800
-FRAMERATE = 17
+FRAMERATE = 30
 COLORS = ['blue','red','black','cyan','magenta','green','blue','yellow','grey','brown','lime','teal','pink']
 OBJECTS = []
 
@@ -21,11 +22,6 @@ w.resizable(False,False)
 # //CHILD WINDOW
 
 #Child W. methods
-
-#  Slider
-def slider_x(val):
-    global Gravity
-    Gravity = int(val)
 def add_ball():
     global OBJECTS
     temp = Objects.Ball(f"ball{len(OBJECTS)+1}",c)
@@ -40,23 +36,20 @@ w_controls = Toplevel(w)
 w_controls.geometry('400x600')
 w_controls.title(f'{w.title()} - Controls')
 w_controls.resizable(False,False)
-ball_gravity_x = Scale(
+velocity_x = Scale(
     w_controls,
-    from_=-100,
+    from_=0,
     to=100,
     orient='horizontal',
-    label='Velocity (x)',
-    command=slider_x
-
+    label='Velocity (x)'
 )
 
-ball_gravity_y = Scale(
+velocity_y = Scale(
     w_controls,
-    from_=-100,
+    from_=0,
     to=100,
     orient='horizontal',
-    label='Velocity (y)',
-    command=slider_x
+    label='Velocity (y)'
 
 )
 btn_add_ball = Button(
@@ -65,10 +58,8 @@ btn_add_ball = Button(
     command=add_ball
 )
 
-ball_gravity_x.set(10)
-ball_gravity_y.set(10)
-ball_gravity_x.pack()
-ball_gravity_y.pack()
+velocity_x.pack()
+velocity_y.pack()
 btn_add_ball.pack()
 
 #  Window Methods
@@ -91,16 +82,20 @@ def exit_app():
 
 def check_edge_hit(obj):
     obj.get_center_pos()
-    posx_adjust = ball_gravity_x.get()
-    posy_adjust = ball_gravity_y.get()
+    posx_adjust = velocity_x.get()
+    posy_adjust = velocity_y.get()
 
     
     #  Check for edges
+
+    # X
+    # Screen Border checks
     if obj.posx + obj.size == W_WIDTH:
         obj.reverse_x = True
     elif obj.posx == 0:
         obj.reverse_x = False
-    elif obj.posx + obj.size > W_WIDTH:
+    # Check for hits that would miss the screen border
+    elif (obj.posx + obj.size + posx_adjust) > W_WIDTH:
         posx_adjust = (obj.posx + obj.size) - W_WIDTH
         obj.reverse_x = True
     elif obj.posx - posx_adjust < 0:
@@ -108,24 +103,27 @@ def check_edge_hit(obj):
         obj.posx = 0
         obj.reverse_x = False
 
+    # Y
+    # Screen Border checks
     if obj.posy + obj.size == W_HEIGHT:
         obj.reverse_y = True
     elif obj.posy == 0:
         obj.reverse_y = False
-    elif obj.posy + obj.size > W_HEIGHT:
-        posy_adjust = (obj.posy + obj.size) - W_HEIGHT
+    # Check for other hits
+    elif (obj.posy + obj.size + posy_adjust) > W_HEIGHT:
+        posy_adjust = 0
+        obj.posy = W_HEIGHT - obj.size
         obj.reverse_y = True
     elif obj.posy - posy_adjust < 0:
         posy_adjust = 0
         obj.posy = 0
         obj.reverse_y = False
 
-    if obj.posx > W_WIDTH or obj.posx < 0: 
+    if obj.posx+10 > W_WIDTH or obj.posx < 0: 
         obj.posx = 250
+        print(f"{obj.tag} reset pos")
     if obj.posy > W_HEIGHT or obj.posy < 0:
         obj.posy = 250
-
-
 
     if obj.reverse_x:
         obj.posx -= posx_adjust
@@ -152,7 +150,6 @@ def update():
     #  Hitbox code
     for object in OBJECTS:
         check_edge_hit(object)
-
 
 def loop():
     update()
