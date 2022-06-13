24d9303efc1336b200f3b09878728cc5aec8e376
Niksld <44138297+Niksld@users.noreply.github.com>
2022-06-13 07:52:46 +0200

    Created size_x and size_y for non-symetric objects
    smrdis

--- Ball.py
+++ Ball.py
@@ -7,16 +7,17 @@ class Ball:
         self.color = 'red'
         self.c = canvas_reference
         self.tag = tag
-        self.size=50
+        self.size_x=50
+        self.size_y=self.size_x
         # (x,y) - tuple
         self.centered_pos = self.get_center_pos()
         #self.centered_pos = ((self.posx+self.size)/2,(self.posy+self.size)/2)
 
     def draw_to(self,x:int, y:int) -> None:
-        self.c.create_oval(x,y,x+self.size,y+self.size, fill=self.color, tag=self.tag)
+        self.c.create_oval(x,y,x+self.size_x,y+self.size_y, fill=self.color, tag=self.tag)
 
     def get_center_pos(self) -> None:
-        self.centered_pos = ((self.posx+self.size)/2,(self.posy+self.size)/2)
+        self.centered_pos = ((self.posx+self.size_x)/2,(self.posy+self.size_y)/2)
 
     def draw(self) -> None:
-        self.c.create_oval(self.posx,self.posy,self.posx+self.size,self.posy+self.size, fill=self.color, tag=self.tag)
+        self.c.create_oval(self.posx,self.posy,self.posx+self.size_x,self.posy+self.size_y, fill=self.color, tag=self.tag)
